4dd426d07c4123e9d4063b59fece571894f7b47a
muhammad zubair <muhammadzubair220@gmail.com>
2025-09-19 15:50:59 +0200

    feat: add .gitignore to exclude sensitive files

--- output.tf
+++ output.tf
@@ -1,16 +1,30 @@
-output "cluster_id" {
-  value = aws_eks_cluster.securezee.id
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.securezee.name
 }
 
-output "node_group_id" {
-  value = aws_eks_node_group.securezee.id
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = aws_eks_cluster.securezee.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_eks_cluster.securezee.vpc_config[0].cluster_security_group_id
+}
+
+output "kubectl_config" {
+  description = "kubectl config command"
+  value       = "aws eks update-kubeconfig --region us-east-1 --name ${aws_eks_cluster.securezee.name}"
 }
 
 output "vpc_id" {
-  value = aws_vpc.securezee_vpc.id
+  description = "VPC ID"
+  value       = aws_vpc.securezee_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.securezee_subnet[*].id
+  description = "Subnet IDs"
+  value       = aws_subnet.securezee_subnet[*].id
 }
 
